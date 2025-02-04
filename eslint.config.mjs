42c115b5ea2a95b43d28613cf549e6ed8019aaef
Vanessa Fernandes <1232495@isep.ipp.pt>
2025-02-04 15:39:51 +0000

    chore: installed prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      complexity: ['error', 5],
+      camelcase: ['error', { properties: 'always' }],
+      'no-unused-vars': ['warn', { vars: 'all', args: 'all' }],
+      'no-console': ['warn'],
+    },
+  },
+  {
+    extends: ['plugin:cypress/recommended'],
+    env: {
+      'cypress/globals': true,
+    },
+  },
+];
